471eaac13b8cfedd4f29ae4067c0e8aa955d0e9a
Katarina Tuttle <katka.karolcikova@gmail.com>
2016-09-22 13:54:14 -0700

    add paperclip for user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController<ApplicationController
   def index
-    
+
   end
-
-
+#
+# private
+#   def sign_up_params
+#     params.require(:user).permit(:email, :avatar, :password, :password_confirmation)
+#   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,20 @@ class UsersController < ApplicationController
     @user = current_user
     render :profile
   end
+
+  def edit
+    @user.update(image_params)
+    # @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(image_params)
+    redirect_to user_path(@user)
+  end
+
+private
+  def image_params
+    params.require(:user).permit(:avatar)
+  end
 end
